7c57785c2e8e32f075a5db61b9787be8122b3e11
cusgadmin <laielli@berkeley.edu>
2018-05-03 16:50:07 -0700

    turned off multiGPUs

--- pytorch-classification/cbas.py
+++ pytorch-classification/cbas.py
@@ -165,7 +165,8 @@ def main():
         model = models.__dict__[args.arch](num_classes=num_classes)
 
     if use_cuda:
-        model = torch.nn.DataParallel(model).cuda()
+        #model = torch.nn.DataParallel(model).cuda()
+        model = model.cuda()
         cudnn.benchmark = True
 
     print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
