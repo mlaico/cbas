f040856e0ba23f46a530852af4efb7f732353dd1
cusgadmin <laielli@berkeley.edu>
2018-05-03 22:38:59 -0700

    adapted cifar.py

--- pytorch-classification/cifar.py
+++ pytorch-classification/cifar.py
@@ -165,7 +165,11 @@ def main():
     else:
         model = models.__dict__[args.arch](num_classes=num_classes)
 
-    model = torch.nn.DataParallel(model).cuda()
+    if use_cuda:
+        #model = torch.nn.DataParallel(model).cuda()
+        model = model.cuda()
+        cudnn.benchmark = True
+
     cudnn.benchmark = True
     print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
 
