98bcee3feaeb2d54617af212d0c98b7e204d60b5
mlai <mjlaielli@gmail.com>
2018-05-07 00:19:57 -0700

    added shell scripts for uzipping images

---
+++ images/setup_images.sh
@@ -0,0 +1,7 @@
+unzip -qq cbas34_train/airplane2donut.zip -d cbas34_train
+unzip -qq cbas34_train/fork2vase.zip -d cbas34_train
+unzip -qq cbas34_val.zip
+
+rm -rf cbas34_train/airplane2donut.zip
+rm -rf cbas34_train/fork2vase.zip
+rm -rf cbas34_val.zip
